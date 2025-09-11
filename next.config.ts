1be52488bd29c7293130582b48cd4ca04b7648d0
xiaoshangfei <fei523688393@hotmail.com>
2025-09-11 17:04:23 +0800

    🎉 init:

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    NEXT_PUBLIC_SILICONFLOW_API_KEY: 'sk-ksmomyhjwefawpcvchmjzmsuehebpyplujarssxipxixemtl',
+    NEXT_PUBLIC_API_BASE_URL: 'https://api.siliconflow.cn/v1',
+    NEXT_PUBLIC_API_MODEL: 'deepseek-ai/DeepSeek-R1-0528-Qwen3-8B'
+  }
 };
 
 export default nextConfig;
